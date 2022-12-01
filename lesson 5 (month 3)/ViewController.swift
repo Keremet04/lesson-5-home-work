d9ba5dffcca2aa4031b1a5322932d87a78db4c26
Keremet Dogdurbaeva <dogdurbaeva04@bk.ru>
2022-12-01 20:24:13 +0600

    Initial Commit

---
+++ lesson 5 (month 3)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson 5 (month 3)
+//
+//  Created by Керемет  on 1/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
